d38a0c278f5485abd885ba1ac33f36244a246074
kepya <58195511+kepya@users.noreply.github.com>
2020-06-03 23:08:56 +0200

    Update MobileElements.java

--- BoulderDash/JPU-BlankProject/entity/src/com/entity/mobileelements/MobileElements.java
+++ BoulderDash/JPU-BlankProject/entity/src/com/entity/mobileelements/MobileElements.java
@@ -1,7 +1,106 @@
 package com.entity.mobileelements;
 
-public class MobileElements {
+import java.io.IOException;
 
+import com.collisionshandler.CollisionsHandler;
+import com.entity.motionlesselements.Path;
+
+import entity.Entity;
+import entity.Sprite;
+
+/**
+ * The Abstract Class MobileElements.
+ *
+ * @author Christian
+ * @author Eliphaz
+ * @version 1.0
+ */
+
+public abstract class MobileElements extends Entity {
+	
+	/** The diamond counter */
+	private int diamondsCounter;
+	
+	/** The boolean to indicates if the mobile element is alive */
+	protected boolean isAlive;
+	
+	/** The death sprite for mobile entities */
+	protected static final Sprite spriteDeath = new Sprite('y', "Death.png");
+	
+	
+	/**
+	 * The MobileElements constructor
+	 * @param sprite the default sprite of the entity
+	 * @param x the initial entity's x on the map
+	 * @param y the initial entity's y on the map
+	 */
+	
+	public MobileElements(Sprite sprite, int x, int y) {
+		super(sprite, x, y);
+	}
+
+	/**
+	 * The entityMove method.
+	 * Allows mobile entities to move on the map.
+	 * @param x the x the entity wants to go (1 goes for right and -1 for left)
+	 * @param y the y the entity wants to go (1 goes for down and -1 for up)
+	 * @param sideX the side the entity wants to move a stone
+	 * @param direction the char that indicates to load the entity's specific sprite
+	 */
+	
+	public void entityMove(int x, int y, int sideX, char direction) {
+
+		final int xpos = this.getPositionX();
+		final int ypos = this.getPositionY();
+		final Entity[][] loadArrayMap = this.getMap().getArrayMap();
+		final Player player = this.getMap().getPlayer();
+		final CollisionsHandler getCollisionHandler = this.getMap().getCollisionsHandler();
+		boolean collision = false;
+		boolean isDiamond = false;
+		boolean moveStone = false;
+		boolean isPlayer = false;
+		
+			if(this instanceof Player) {
+				collision = getCollisionHandler.checkForCollisions(loadArrayMap,xpos + x, ypos + y);
+				isDiamond = getCollisionHandler.checkForDiamonds(loadArrayMap, xpos + x,ypos + y);
+				moveStone = getCollisionHandler.checkForStoneToMove(loadArrayMap, xpos + x, ypos + y, sideX);
+			}else {
+				collision = getCollisionHandler.checkForPath(loadArrayMap,xpos + x, ypos + y);
+				isPlayer = getCollisionHandler.checkForPlayer(loadArrayMap, xpos + x, ypos + y);
+			}
+				
+
+			this.loadImage(direction, this);
+			
+			if(moveStone) {
+				loadArrayMap[xpos + x + sideX][ypos + y] = loadArrayMap[xpos + x][ypos + y];
+				loadArrayMap[xpos + x][ypos + y] = loadArrayMap[xpos][ypos];
+				loadArrayMap[xpos][ypos] = new Path(xpos, ypos);
+				this.setPositionY(ypos + y);
+				this.setPositionX(xpos + x);
+				loadArrayMap[xpos + x + sideX][ypos + y].setPositionX(xpos + x + sideX);
+			}
+			
+			if (!collision) {
+
+				if(isPlayer == true) {
+					player.setIsAlive(false);
+				} else {
+					loadArrayMap[xpos + x][ypos + y] = loadArrayMap[xpos][ypos];
+					loadArrayMap[xpos][ypos] = new Path(xpos, ypos);
+					this.setPositionY(ypos + y);
+					this.setPositionX(xpos + x);
+				}
+				
+			}
+
+			if (isDiamond == true) {
+				this.incrementDiamondsCounter();
+			}
+			
+
+			
+	}
 	/**
 	 * The loadImage method.
 	 * Allows the entity to change sprite on the map depending on its movements.
