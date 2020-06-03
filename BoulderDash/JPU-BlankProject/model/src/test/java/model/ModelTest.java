b4ec8054b9b7680021d933fa026b1545df7e3683
kepya <58195511+kepya@users.noreply.github.com>
2020-06-03 23:40:59 +0200

    Update ModelTest

--- BoulderDash/JPU-BlankProject/model/src/test/java/model/ModelTest.java
+++ BoulderDash/JPU-BlankProject/model/src/test/java/model/ModelTest.java
@@ -1,55 +1,87 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
-
-import java.sql.Connection;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import contract.IController;
-import contract.IView;
-
-
+/**
+ * The ModelTest class.
+ * @author Christian
+ * @author Eliphaz
+ * @version 1.0
+ *
+ */
 public class ModelTest {
+	
+	/** The model for the tests */
+    private Model model;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-	private Model model;
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		model = new Model();
-		
-	}
+    /**
+     * Instantiates a new model.
+     * @throws Exception in case of an exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.model = new Model();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testPosition() {
-		model.loadMap(1);
-		int expectedX = 1;
-		int expectedY = 18;
-		assertEquals(expectedX, model.getDoorX());
-		assertEquals(expectedY, model.getDoorY());
-	}
-	@Test
-	public void testPlayer() {
-		model.loadMap(2);
-		int expectedX = 1;
-		int expectedY = 1;
-		assertEquals(expectedX, model.getCharacter().posX);
-		assertEquals(expectedY, model.getCharacter().posY);
-	}
+    @Test
+    public void testContentOfMap() {
+      Assert.assertEquals("", this.model.getMap().getContentOfMap());
+    }
+
+
+    /**
+     * The testGetNumberOfDiamondsNeeded method.
+     * Check that the connection to the database if well-established and the correct number of diamonds is picked.
+     */
+    @Test
+    public void testGetNumberOfDiamondsNeeded() {
+        this.model.loadMap(1);
+        Assert.assertEquals(3, this.model.getMap().getNumberOfDiamondsNeeded());
+        this.model.loadMap(2);
+        Assert.assertEquals(5, this.model.getMap().getNumberOfDiamondsNeeded());
+        this.model.loadMap(3);
+        Assert.assertEquals(7, this.model.getMap().getNumberOfDiamondsNeeded());
+        this.model.loadMap(4);
+        Assert.assertEquals(9, this.model.getMap().getNumberOfDiamondsNeeded());
+        this.model.loadMap(5);
+        Assert.assertEquals(10, this.model.getMap().getNumberOfDiamondsNeeded());
+    }
+    
+    /**
+     * Test the limit of level loading. If there is an exception, assertEquals will return true
+     */
+    @Test
+    public void testLimitLoadingMap() {
+
+        try {
+        	this.model.loadMap(8);
+            fail("Failed");
+        } catch (IndexOutOfBoundsException e) {
+            assertEquals(e.getMessage(), "Incorrect id !"); 
+        }
+    }
+    
+    
+    
 
 }
