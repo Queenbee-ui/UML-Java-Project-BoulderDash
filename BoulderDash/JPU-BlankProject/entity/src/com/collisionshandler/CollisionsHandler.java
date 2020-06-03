358322dc11c1f0faad682ef538fff8cd7db40933
kepya <58195511+kepya@users.noreply.github.com>
2020-06-03 22:53:23 +0200

    Update CollisionsHandler.java
    Update of CollisionHandler.java

--- BoulderDash/JPU-BlankProject/entity/src/com/collisionshandler/CollisionsHandler.java
+++ BoulderDash/JPU-BlankProject/entity/src/com/collisionshandler/CollisionsHandler.java
@@ -50,5 +50,69 @@ public class CollisionsHandler {
 			return false;
 		}
 	}
+	/**
+	 * The checkForDiamonds method.
+	 * @param playerPosition the player
+	 * @param x the player's x
+	 * @param y the player's y
+	 * @return a boolean that indicates if the player is going on a diamond.
+	 */
+	
+	public boolean checkForDiamonds(Entity[][] playerPosition, int x, int y) {
+
+		if (playerPosition[x][y] instanceof Diamond) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * The checkForPlayer method.
+	 * @param entity the entity that is gonna kill or no the player
+	 * @param x the entity's x
+	 * @param y the entity's y
+	 * @return a boolean that indicates if there is a player near the entity.
+	 */
+	
+	public boolean checkForPlayer(Entity[][] entity, int x, int y) {
+
+		if (entity[x][y] instanceof Player) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * The checkForPath method.
+	 * @param entity the entity
+	 * @param x the entity's x
+	 * @param y the entity's y
+	 * @return a boolean that indicates if there is a path near the entity. If true, it can moves.
+	 */
+	
+	public boolean checkForPath(Entity[][] entity, int x, int y) {
+
+		if (entity[x][y] instanceof Path) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * The checkForStoneToMove method.
+	 * @param entity the player
+	 * @param x the player's x
+	 * @param y the player's y
+	 * @param sideX the side on which the player wants to move the stone (left/right)
+	 * @return a boolean that indicates if the player can move a stone on the side he wants.
+	 */
+	
+	public boolean checkForStoneToMove(Entity[][] entity, int x, int y, int sideX) { 
+
+		if (entity[x][y] instanceof Stone && entity[x+sideX][y] instanceof Path) {
+			return true;
+		}
+		return false;
+	}
 
 }
